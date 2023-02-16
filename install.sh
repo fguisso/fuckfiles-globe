8b7a2cedc5e30942f651ff8cd5c3df36f2236510
Fernando Guisso <fguisso@gmail.com>
2023-02-16 14:33:28 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ set -e
 apt install -y openconnect
 
 # Tsuru
-curl -s https://packagecloud.io/install/repositories/tsuru/stable/script.deb.sh | bash
-sudo apt-get install tsuru-client
-tsuru target-add default --set-current $TSURU_TARGET
+# curl -s https://packagecloud.io/install/repositories/tsuru/stable/script.deb.sh | bash
+# sudo apt-get install tsuru-client
+# tsuru target-add default --set-current $TSURU_TARGET
